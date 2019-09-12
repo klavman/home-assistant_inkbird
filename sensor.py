b49a881488d5160272253288150324066855c340
Fco. Javier Clavero Álvarez <jclaveroalvarez@gmail.com>
2019-09-12 13:31:06 +0200

    change seconds update

--- sensor.py
+++ sensor.py
@@ -17,7 +17,7 @@ import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=5)
 
 DEFAULT_NAME = 'Inkbird'
 
